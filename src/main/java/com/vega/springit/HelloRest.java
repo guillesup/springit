5214719a310bd05c78984075eb01b7fe39746deb
Guillermo Guzmán Sánchez <guillermo.guzman@zabud.com.co>
2020-12-13 16:51:00 -0500

    first commit

---
+++ src/main/java/com/vega/springit/HelloRest.java
@@ -0,0 +1,12 @@
+package com.vega.springit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRest {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello I'm looking for a hot, chubby and dirty girl!";
+    }
+}
